8abe27b49b35c78b415090389097d5520224b481
Sami Munir <sami.munir2001@gmail.com>
2024-11-04 23:04:27 -0500

    starting data structures & algo repo: dynamic-array (java implementation)

---
+++ Data Structures and Algorithms/Java Programming/Dynamic Array/dynamic-array/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nDynamic Array - Java Implementation");
+        System.out.println("Data Structures & Algorithms");
+        System.out.println("-----------------------------------\n");
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,7 @@
 Sami Munir | sami.munir2001@gmail.com
 # codespace
+## Data Structures & Algorithms
+### Java Programming
+#### Dynamic Array
+### C Programming
+### Python Programming
