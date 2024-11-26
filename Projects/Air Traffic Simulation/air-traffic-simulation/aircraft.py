931b44df737d7e7afbb5bcf2b261ecffe5e4e717
Sami Munir <sami.munir2001@gmail.com>
2024-11-26 00:14:00 -0500

    implementing Aircraft.py logic to draw heading vector from aircraft + heat map trail

--- Projects/Air Traffic Simulation/air-traffic-simulation/aircraft.py
+++ Projects/Air Traffic Simulation/air-traffic-simulation/aircraft.py
@@ -6,9 +6,13 @@ from config import (
     AIRCRAFT_WIDTH,
     AIRCRAFT_HEIGHT,
     AIRCRAFT_SPEED_SCALAR,
+    AIRCRAFT_HEADING_VECTOR_LENGTH,
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
 )
+
+PG.init()
+font = PG.font.Font(None, 18)
 
 class Aircraft:
     def __init__(self, flight_number: str, type: str, x: float, y: float, speed: float, altitude: float, heading: float):
@@ -23,11 +27,19 @@ class Aircraft:
         self.last_trail_position = (self.x, self.y)
         self.selected = False
     
-    def draw(self, screen):
+    def draw(self, screen, label_color):
+        heading_vector_color = label_color
+        aircraft_label_1 = f'{self.flight_number} - {self.type}'
+        aircraft_label_2 = f'{self.speed}kts {self.altitude}ft'
+        aircraft_label_3 = f'{int(self.heading)}°'
+        self.draw_text(screen, aircraft_label_1, font, label_color, offset=(AIRCRAFT_WIDTH / 2, -60))
+        self.draw_text(screen, aircraft_label_2, font, label_color, offset=(AIRCRAFT_WIDTH / 2, -40))
+        self.draw_text(screen, aircraft_label_3, font, label_color, offset=(AIRCRAFT_WIDTH / 2, -20))
         left = self.x - (AIRCRAFT_WIDTH / 2)
         top = self.y - (AIRCRAFT_HEIGHT / 2)
         aircraft_rectangle = (left, top, AIRCRAFT_WIDTH, AIRCRAFT_HEIGHT)
         PG.draw.rect(surface = screen, color = AIRCRAFT_COLOR, rect = aircraft_rectangle)
+        self.draw_heading_vector(screen, heading_vector_color)
         if self.selected:
             PG.draw.circle(screen, (255, 255, 0), (int(self.x), int(self.y)), 25, 2)
         self.draw_trail(screen)
@@ -36,68 +48,71 @@ class Aircraft:
         if self.heading == 0 or self.heading == 360:
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 15:
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 30:
+            self.x += self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * (AIRCRAFT_SPEED_SCALAR)
         elif self.heading == 45:
             self.x += self.speed * AIRCRAFT_SPEED_SCALAR
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 60:
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y -= self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
         elif self.heading == 75:
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y -= self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
         elif self.heading == 90:
             self.x += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 105:
-            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y += self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
+        elif self.heading == 120:
+            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y += self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
         elif self.heading == 135:
             self.x += self.speed * AIRCRAFT_SPEED_SCALAR
             self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 150:
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
+            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 165:
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x += self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
+            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 180:
             self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 195:
+            self.x -= self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
             self.y += self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 210:
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x -= self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
+            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 225:
             self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
             self.y += self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 240:
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
-        elif self.heading == 265:
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
-            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y += self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
+        elif self.heading == 255:
+            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y += self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
         elif self.heading == 270:
             self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 285:
-            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
-        elif self.heading == 300:
-            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
             self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y -= self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
+        elif self.heading == 300:
+            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.y -= self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
         elif self.heading == 315:
             self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 330:
-            self.x -= self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x -= self.speed * (AIRCRAFT_SPEED_SCALAR / 2)
             self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         elif self.heading == 345:
-            self.y += self.speed * AIRCRAFT_SPEED_SCALAR
-            self.x += self.speed * AIRCRAFT_SPEED_SCALAR
+            self.x -= self.speed * (AIRCRAFT_SPEED_SCALAR / 3)
+            self.y -= self.speed * AIRCRAFT_SPEED_SCALAR
         else:
             pass
         
@@ -109,7 +124,7 @@ class Aircraft:
         if distance > trail_spacing:
             self.trail.append((self.x, self.y))
             self.last_trail_position = (self.x, self.y)
-        if (len(self.trail) > 5000):
+        if (len(self.trail) > 1000):
             self.trail.pop(0)
     
     def draw_text(self, screen, text, font, color, offset = (0, -30)):
@@ -129,3 +144,15 @@ class Aircraft:
     def is_clicked(self, mouse_pos):
         distance = MATH.sqrt((mouse_pos[0] - self.x) ** 2 + (mouse_pos[1] - self.y ) ** 2)
         return distance < 20
+    
+    def draw_heading_vector(self, screen, color):
+        start_x = self.x
+        start_y = self.y
+        
+        # Convert heading from degrees to radians
+        heading_radian = MATH.radians(self.heading)
+        
+        end_x = start_x + MATH.sin(heading_radian) * AIRCRAFT_HEADING_VECTOR_LENGTH
+        end_y = start_y - MATH.cos(heading_radian) * AIRCRAFT_HEADING_VECTOR_LENGTH
+        
+        PG.draw.line(screen, color, (start_x, start_y), (end_x, end_y), 2)
--- Projects/Air Traffic Simulation/air-traffic-simulation/main.py
+++ Projects/Air Traffic Simulation/air-traffic-simulation/main.py
@@ -1,9 +1,3 @@
-#         # event handling
-#         # keys = pygame.key.get_pressed()
-#         # if keys[pygame.K_LEFT]:
-#         #     aircraft.turn('left')
-#         # if keys[pygame.K_RIGHT]:
-#         #     aircraft.turn('right')
 import pygame as PG
 from config import (
     SCREEN_WIDTH,
@@ -20,41 +14,49 @@ def main():
     PG.display.set_caption('Air Traffic Simulation')
     CLOCK = PG.time.Clock()
     
-    font = PG.font.Font(None, 18)
+    AIRCRAFTS = []
     
-    aircraft = Aircraft(flight_number = 'N147SM', type = 'C172', x = 450, y = 450, speed = 95, altitude = 2500, heading = 0)
+    aircraft_a = Aircraft(flight_number = 'N147SM', type = 'C172', x = 450, y = 450, speed = 95, altitude = 2500, heading = 0)
+    aircraft_b = Aircraft(flight_number = 'JBU315', type = 'A20N', x = 600, y = 750, speed = 250, altitude = 8000, heading = 0)
+    aircraft_c = Aircraft(flight_number = 'AAL527', type = 'B38M', x = 200, y = 200, speed = 300, altitude = 22000, heading = 0)
+    
+    AIRCRAFTS.append(aircraft_a)
+    AIRCRAFTS.append(aircraft_b)
+    AIRCRAFTS.append(aircraft_c)
     
     RUNNING = True
     while RUNNING:
         SCREEN.fill(BACKGROUND_COLOR)
         
         for EVENT in PG.event.get():
-            if EVENT.type == PG.KEYDOWN and EVENT.key == PG.K_LEFT:
-                if (aircraft.heading - 90 < 0):
-                    aircraft.heading = 360
-                aircraft.heading -= 90
-                # print('left pressed')
-            if EVENT.type == PG.KEYDOWN and EVENT.key == PG.K_RIGHT:
-                if (aircraft.heading + 90 > 360):
-                    aircraft.heading = 0
-                aircraft.heading += 90
-                # print('right pressed')
-            
+            if EVENT.type == PG.MOUSEBUTTONDOWN:
+                mouse_pos = PG.mouse.get_pos()
+                for aircraft in AIRCRAFTS:
+                    aircraft.selected = aircraft.is_clicked(mouse_pos)
+            for aircraft in AIRCRAFTS:
+                if aircraft.selected:
+                    if EVENT.type == PG.KEYDOWN and EVENT.key == PG.K_LEFT:
+                        if (aircraft.heading - 15 < 0):
+                            aircraft.heading = 360
+                        aircraft.heading -= 15
+                    if EVENT.type == PG.KEYDOWN and EVENT.key == PG.K_RIGHT:
+                        if (aircraft.heading + 15 > 360):
+                            aircraft.heading = 0
+                        aircraft.heading += 15
             if EVENT.type == PG.KEYDOWN:
                 if EVENT.key == PG.K_ESCAPE:
                     RUNNING = False
             if EVENT.type == PG.QUIT:
                 RUNNING = False
         
-        aircraft.move()
-        aircraft.draw(screen = SCREEN)
-        # \n{aircraft.speed}kts - {aircraft.altitude}ft\n{int(aircraft.heading)}°
-        aircraft_label_1 = f'{aircraft.flight_number} - {aircraft.type}'
-        aircraft_label_2 = f'{aircraft.speed}kts {aircraft.altitude}ft'
-        aircraft_label_3 = f'{int(aircraft.heading)}°'
-        aircraft.draw_text(SCREEN, aircraft_label_1, font, (255, 255, 255), offset=(AIRCRAFT_WIDTH / 2, -60))
-        aircraft.draw_text(SCREEN, aircraft_label_2, font, (255, 255, 255), offset=(AIRCRAFT_WIDTH / 2, -40))
-        aircraft.draw_text(SCREEN, aircraft_label_3, font, (255, 255, 255), offset=(AIRCRAFT_WIDTH / 2, -20))
+        for aircraft in AIRCRAFTS:
+            if aircraft.x < 0 or aircraft.x > SCREEN_WIDTH or aircraft.y < 0 or aircraft.y > SCREEN_HEIGHT:
+                AIRCRAFTS.remove(aircraft)
+            label_color =  (255, 255, 255)
+            if aircraft.selected:
+                label_color = (255, 255, 0)
+            aircraft.move()
+            aircraft.draw(screen = SCREEN, label_color = label_color)
         
         PG.display.flip()
         CLOCK.tick(FPS)
