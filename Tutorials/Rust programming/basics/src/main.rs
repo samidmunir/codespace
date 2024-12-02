3c63f0c91441f6965a651be05f2093890818111c
Sami Munir <sami.munir2001@gmail.com>
2024-12-01 22:49:48 -0500

    learning basics of Rust (strongly-static typed lang)

---
+++ Tutorials/Rust programming/basics/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x: u32 = 7;
+    println!("x is: {}", x);
+
+    let mut y: u32 = 0;
+    println!("initial -> y is: {}", y);
+    y = 63;
+    println!("before -> y is: {}", y);
+
+    let mut y: u32 = 89;
+    println!("after -> y is: {}", y);
+}
